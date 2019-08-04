71bad59232919aab9c9e2205fe0cdacf6b3b355b
Fabricio Policarpo <fabriciopolicarpo0@gmail.com>
2019-08-04 19:31:29 -0400

    I hate css

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,8 +1,9 @@
 
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+hbs.registerPartials(__dirname + '/views/partials')
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
@@ -12,10 +13,34 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 
+
 app.get('/', (req, res, next) => {
-  res.render('index');
+  res.render('index.hbs');
 });
 
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+    .then(beers => {
+      console.log(beers[0])
+      res.render('beers.hbs', { beers });
+    })
+    .catch(error => {
+      console.log(error)
+    })
+})
 
-app.listen(3000);
+app.get('/randomBeer', (req, res, next) => {
+  punkAPI.getRandom()
+    .then(beers => {
+      res.render('beerPartials.hbs', beers[0])
+    })
+    .catch(error => {
+      console.log(error)
+    })
+})
+
+
+app.listen(3000, () => {
+  console.log('running on port 3000')
+});
